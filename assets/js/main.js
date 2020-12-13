89722e302ec02e34b18d27c90ef65e3e363703ce
sinika <sinikasinika@gmail.com>
2020-12-13 19:08:08 +0200

    Inicio

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.header-navbar__btn').click(function(){
+    $(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').slideToggle('500');
+  });
+});
